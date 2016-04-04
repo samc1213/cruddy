3787ac76faa60432253088b127c3484983d60560
Sam Cohen <samc1213@gmail.com>
2016-04-03 22:35:12 -0400

    able to change color style and size of font

--- static/js/grid.js
+++ static/js/grid.js
@@ -2,6 +2,29 @@ $(document).ready( function() {
   $('.addgridbtn').click( function() {
     $(this).attr('disabled', 'disabled');
   });
+  for (var i = 5; i < 70; i++){
+    $('#fontsize').append($("<option></option>")
+         .attr("value",i)
+         .text(i));
+    }
+  coloroptions = {'black': 'Black', 'white':'White', 'red':'Red','blue':'Blue','green':'Green','yellow':'Yellow'}
+  for (htmlcolor in coloroptions) {
+    var colortext = coloroptions[htmlcolor];
+    $('#fontcolor').append($("<option></option>")
+         .attr("value",htmlcolor)
+         .text(colortext));
+  }
+  styleoptions = {'normal': 'Normal', 'italic':'Italic',}
+  for (htmlstyle in styleoptions) {
+    var style = styleoptions[htmlstyle];
+    $('#fstyle').append($("<option></option>")
+         .attr("value",htmlstyle)
+         .text(style));
+  }
+  $(document).on('click', '.gs-w', function(e) {
+    highlight($(e.target));
+  });
+  $('#customfontarea').change(updatefontstuff);
   var url =window.location.href;
   var urlsplitarr = url.split('/');
   var ename = urlsplitarr[urlsplitarr.length - 1];
@@ -20,7 +43,7 @@ $(document).ready( function() {
         max_cols: numcols,
         min_cols: numcols,
         resize: {enabled: true},
-        serialize_params: function($w, wgd) { return { col: wgd.col, row: wgd.row, size_x: wgd.size_x, size_y: wgd.size_y, name:$w.text(), fieldnamenumber:$w.attr('fieldnamenumber')} }
+        serialize_params: function($w, wgd) { return { col: wgd.col, row: wgd.row, size_x: wgd.size_x, size_y: wgd.size_y, name:$w.text(), fieldnamenumber:$w.attr('fieldnamenumber'), fontsize:$w.attr('fontsize'), fontcolor:$w.attr('fontcolor'), fstyle:$w.attr('fstyle')} }
     }).data('gridster');
     if (gridjson != '') {
       var json = jQuery.parseJSON(gridjson);
@@ -47,7 +70,9 @@ $(document).ready( function() {
       console.log($(this).attr('id'));
       if ($(this).attr('id') != ""){
         $(this).click(function(){
-            gridsteryo.add_widget('<li fieldnamenumber="' + $(this).attr('fieldnamenumber') + '">'+$(this).attr('id').substr(3)+'</li>', 6, 2);
+          console.log('bullshit');
+            gridsteryo.add_widget('<li fstyle="normal" fontcolor="black" fontsize="12" fieldnamenumber="' + $(this).attr('fieldnamenumber') + '">'+$(this).attr('id').substr(3)+'</li>', 6, 2);
+            highlight($('li[fieldnamenumber="' + $(this).attr('fieldnamenumber') + '"]'));
         });
       }
       //fix this shit
@@ -79,9 +104,27 @@ function addnewfield() {
 function savegrid() {
   var gridster = $(".gridster ul").gridster().data('gridster');
   var gridjson = JSON.stringify(gridster.serialize());
+  console.log('GRIDJAYFORSAVE'  + gridjson);
   var url =window.location.href;
   var urlsplitarr = url.split('/');
   var ename = urlsplitarr[urlsplitarr.length - 1]
   $.post( "/savegrid/" + ename, { gridjson: gridjson } );
   console.log();
+}
+
+function highlight($gridbox) {
+  $('#curfieldspan').text($gridbox.text());
+  $('#fontsize').val($gridbox.attr('fontsize'));
+  $('#fontcolor').val($gridbox.attr('fontcolor'));
+    $('#fstyle').val($gridbox.attr('fstyle'));
+  $('#currentboxfieldnamenum').val($gridbox.attr('fieldnamenumber'));
+  console.log('highlight');
+  updatefontstuff();
+}
+
+function updatefontstuff() {
+  $('.gs-w[fieldnamenumber="' + $('#currentboxfieldnamenum').val() + '"]').attr('fontsize', $('#fontsize').val());
+  $('.gs-w[fieldnamenumber="' + $('#currentboxfieldnamenum').val() + '"]').attr('fontcolor', $('#fontcolor').val());
+  $('.gs-w[fieldnamenumber="' + $('#currentboxfieldnamenum').val() + '"]').attr('fstyle', $('#fstyle').val());
+  console.log('update');
 }
